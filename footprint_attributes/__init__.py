a7cc2a61a62e77797247e8e7a502bb6d0c86643d
GeomaticsCaminosUPM <geomatics.caminosupm@gmail.com>
2024-12-22 22:20:11 +0100

    Add files via upload

--- footprint_attributes/__init__.py
+++ footprint_attributes/__init__.py
@@ -1,2 +1,4 @@
+__all__ = ['calc_forces', 'relative_position', 'calc_shape_irregularity', 'calc_polsby_popper']
+
 from .forces import calc_forces, relative_position
 from .irregularity import calc_shape_irregularity, calc_polsby_popper
