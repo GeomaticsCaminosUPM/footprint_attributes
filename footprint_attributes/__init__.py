29793ba582786c42b6362df63f058721483b8c68
GeomaticsCaminosUPM <geomatics.caminosupm@gmail.com>
2024-12-22 22:37:09 +0100

    Add files via upload

--- footprint_attributes/__init__.py
+++ footprint_attributes/__init__.py
@@ -1,4 +1,13 @@
-__all__ = ['calc_forces', 'relative_position', 'calc_shape_irregularity', 'calc_polsby_popper']
-
 from .forces import calc_forces, relative_position
 from .irregularity import calc_shape_irregularity, calc_polsby_popper
+
+__all__ = ['calc_forces', 'relative_position', 'calc_shape_irregularity', 'calc_polsby_popper']
+
+# Explicitly hide the 'forces' module to prevent it from being directly imported
+import sys
+import warnings
+
+# Delete 'forces' from the globals to ensure it cannot be accessed
+del sys.modules['.forces']
+del sys.modules['.irregularity']
+del sys.modules['.utils']
