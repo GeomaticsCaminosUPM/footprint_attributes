0845bc94226edd2b925ae64bfb101ee29336b8fb
GeomaticsCaminosUPM <geomatics.caminosupm@gmail.com>
2025-02-02 11:17:27 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="footprint_attributes",
-    version="1.0.0",
+    version="0.2.0",
     description="",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
