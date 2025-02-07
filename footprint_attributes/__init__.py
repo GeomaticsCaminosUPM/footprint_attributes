3871b4afc58c7c7968b70c2ef07aecf9bd0d795a
GeomaticsCaminosUPM <geomatics.caminosupm@gmail.com>
2025-02-07 17:40:25 +0100

    Update __init__.py

--- footprint_attributes/__init__.py
+++ footprint_attributes/__init__.py
@@ -1,9 +1,9 @@
 from ._forces import get_forces_gdf, relative_position
-from ._irregularity import shape_irregularity, polsby_popper, inertia_circle, inertia_slenderness, compactness
+from ._irregularity import shape_irregularity, polsby_popper, inertia_circle, inertia_slenderness, compactness, excentricity_ratio
 import sys
 
 __version__ = '0.2.0'
-__all__ = ['get_forces_gdf', 'relative_position', 'shape_irregularity', 'polsby_popper', 'inertia_circle','inertia_slenderness','compactness','__version__']
+__all__ = ['get_forces_gdf', 'relative_position', 'shape_irregularity', 'polsby_popper', 'inertia_circle','inertia_slenderness','excentricity_ratio','compactness','__version__']
 
 
 # Remove the private submodules from the package namespace
