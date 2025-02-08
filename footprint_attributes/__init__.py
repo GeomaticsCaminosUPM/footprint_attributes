ca6aa8f0cdfd3c01f42f9427126c183e721413d4
GeomaticsCaminosUPM <geomatics.caminosupm@gmail.com>
2025-02-08 19:26:31 +0100

    Update __init__.py

--- footprint_attributes/__init__.py
+++ footprint_attributes/__init__.py
@@ -1,9 +1,34 @@
 from ._forces import get_forces_gdf, relative_position
-from ._irregularity import shape_irregularity, polsby_popper, inertia_circle, inertia_slenderness, compactness, excentricity_ratio
+from ._irregularity import (
+    shape_irregularity,
+    polsby_popper, 
+    inertia_circle,
+    inertia_slenderness,
+    compactness,
+    setback_ratio,
+    hole_ratio,
+    get_eurocode_8_irregularity,
+    get_costa_rica_irregularity,
+    get_mexico_NTC_irregularity
+)
+
 import sys
 
-__version__ = '0.2.0'
-__all__ = ['get_forces_gdf', 'relative_position', 'shape_irregularity', 'polsby_popper', 'inertia_circle','inertia_slenderness','excentricity_ratio','compactness','__version__']
+__version__ = '0.3.0'
+__all__ = ['get_forces_gdf',
+           'relative_position', 
+           'convex_hull_momentum',
+           'polsby_popper',
+           'inertia_circle'
+           'inertia_slenderness',
+           'excentricity_ratio',
+           'compactness',
+           'setback_ratio',
+           'hole_ratio',
+           'get_eurocode_8_irregularity',
+           'get_costa_rica_irregularity',
+           'get_mexico_NTC_irregularity',
+           '__version__']
 
 
 # Remove the private submodules from the package namespace
